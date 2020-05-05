ff6c6ac7def08f6701f62d1cfe22788f45c1b50a
caozhen <wuwangchuxincz@163.com>
2020-05-05 20:34:52 +0800

    day02提交

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   lintOnSave: false, // 关闭eslint规则检查
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://182.92.128.115",
+        changeOrigin: true,
+      },
+    },
+  },
 };
